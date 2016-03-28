7d0182c6dc323b685c88944e8f47db139948d029
Anatoly Gusev <avg.tolik@gmail.com>
2016-03-28 22:49:03 +0300

    Redo bottom line of task card

--- client/components/ShowTaskForm.js
+++ client/components/ShowTaskForm.js
@@ -55,13 +55,13 @@ const TaskForm = React.createClass( {
           
           this.setState( obj );
         };
-  
+
       default:
         return ( e ) => {
           let obj = {};
           obj[ field ] = e.target.value;
           console.log( e.target.value );
-  
+
           this.setState( obj );
         };
     }
@@ -94,11 +94,11 @@ const TaskForm = React.createClass( {
       <div className="ui segments">
         <div className="ui card form task-card">
           <div className="content task-card__top-line">
-    
+  
             <div className="ui fluid input task-card__top-input">
               <input placeholder="Task Name..." onChange={this.handleChange('name')} value={this.state.name}/>
             </div>
-    
+  
             <div className="circular ui icon top left pointing dropdown basic button task-card__priority-flag" ref="priorityDropdown">
               <i className={this.getPriorityFlagClass()}/>
               <div className="menu">
@@ -117,24 +117,21 @@ const TaskForm = React.createClass( {
             <button onClick={this.props.onCloseTask} className="circular ui icon basic button task-card__close-button">
               <i className="remove icon"/>
             </button>
-            
+
           </div>
           <div className="content">
             <div className="field">
               <textarea placeholder="Task Description..." onChange={this.handleChange('description')} value={this.state.description}/>
             </div>
           </div>
-          <div className="extra content">
-            <div className="ui grid">
-              <div className="six wide column">
-                <button type="submit" className="ui teal button" tabIndex="0" onClick={this.handleSubmit}>Save Task</button>
-              </div>
-              <div className="ten wide column">
-                <div className="ui right floated calendar inline field datepicker" ref="datepicker">
-                  <label>Due to: </label>
-                  <input onChange={this.handleChange('due_date')} value={moment(this.state.due_date).format( 'MM/DD/YYYY' )}/>
-                </div>
-              </div>
+          <div className="extra content task-card__bottom-line">
+            <div className="task-card__submit-button">
+              <button type="submit" className="ui teal button" tabIndex="0" onClick={this.handleSubmit}>Save Task</button>
+            </div>
+    
+            <div className="task-card__datepicker ui calendar inline field" ref="datepicker">
+              <label>Due to: </label>
+              <input onChange={this.handleChange('due_date')} value={moment(this.state.due_date).format( 'MM/DD/YYYY' )}/>
             </div>
           </div>
         </div>
