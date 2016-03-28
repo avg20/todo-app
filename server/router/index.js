366437e90328807ba87a518ad216f4d8f5aa1a33
Anatoly Gusev <avg.tolik@gmail.com>
2016-03-28 13:50:32 +0300

    Add DELETE route for tasks

--- server/router/index.js
+++ server/router/index.js
@@ -9,7 +9,8 @@ module.exports = function ( app ) {
   app.get( '/users', auth.check, userRoutes.getUsers );
   app.post( '/users', userRoutes.addUser );
   app.post( '/users/login', userRoutes.loginUser );
-
+  
   app.get( '/tasks', auth.check, taskRoutes.getTasks );
   app.post( '/tasks', auth.check, taskRoutes.addTask );
+  app.delete( '/tasks/:id', auth.check, taskRoutes.deleteTask )
 };
--- server/router/routes/task.js
+++ server/router/routes/task.js
@@ -11,23 +11,23 @@ module.exports.getTasks = ( req, res ) => {
   const query = req.query;
   const filters = { user_id: auth.user._id };
   let sort = "";
-
+  
   for ( var field in query )
     if ( query.hasOwnProperty( field ) ) {
       if ( field === 'token' ) continue;
-
+  
       if ( field === 'sort' ) {
         sort = query[ field ].replace( ",", " " );
-
+    
         continue;
       }
-
+  
       filters[ field ] = new RegExp( query[ field ] );
     }
-
+  
   Task.find( filters ).sort( sort ).exec( ( err, docs ) => {
     if ( err ) throw err;
-
+    
     res.json( { status: 'success', tasks: docs } );
   } );
 };
@@ -38,16 +38,32 @@ module.exports.getTasks = ( req, res ) => {
 module.exports.addTask = ( req, res ) => {
   let data = JSON.parse( req.body );
   data.user_id = auth.user._id;
-
+  
   new Task( data ).save( ( err, model ) => {
     if ( err ) {
       const errors = {};
       for ( let i in err.errors ) {
         errors[ i ] = err.errors[ i ].message;
       }
-
+  
       res.json( { status: 'error', errors: errors } );
     } else
       res.json( { status: 'success', task: model } );
+  } );
+};
+
+/**
+ * Delete selected task
+ **/
+module.exports.deleteTask = ( req, res ) => {
+  let query = {};
+  query.user_id = auth.user._id;
+  query._id = req.params.id;
+  
+  Task.remove( query, ( err ) => {
+    if ( err ) {
+      res.json( { status: 'error' } );
+    } else
+      res.json( { status: 'success' } );
   } );
 };
