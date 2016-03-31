2e8bb9ac35dd2650af2f4412f50bbcc254f23bc5
Anatoly Gusev <avg.tolik@gmail.com>
2016-03-31 18:19:03 +0300

    Change server to mark message as read after get route

--- server/router/routes/message.js
+++ server/router/routes/message.js
@@ -8,9 +8,15 @@ const auth = require( '../../auth' );
  * Get list of unread message of current authorized user
  **/
 module.exports.getMessages = ( req, res ) => {
-  Message.find( { user_id: auth.user._id, read: false }, ( err, docs ) => {
+  const query = { user_id: auth.user._id, read: false };
+  
+  Message.find( query, ( err, docs ) => {
     if ( err ) throw err;
     
-    res.json( { status: 'success', messages: docs } );
+    Message.update( query, { read: true }, ( err ) => {
+      if ( err ) throw err;
+      
+      res.json( { status: 'success', messages: docs } );
+    } )
   } );
 };
