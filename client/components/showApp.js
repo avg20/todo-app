87a1298c1fdc34fa88be657326a735bbe3c8fbde
Anatoly Gusev <avg.tolik@gmail.com>
2016-03-31 17:50:17 +0300

    Add showApp

--- client/components/showApp.js
+++ client/components/showApp.js
@@ -4,30 +4,26 @@ import React from 'react';
 import AddTask from '../containers/AddTask';
 import ShowTasks from '../containers/ListTasks';
 import TopBar from '../containers/TopBar';
+import Notify from '../helpers/Notify';
 
 const App = React.createClass( {
   render: function () {
-    if ( !this.props.isSelected )
-      return (
-        <div className="ui main">
-          <TopBar />
-          <ShowTasks />
-        </div>
-      );
-    else
-      return (
-        <div className="ui main stackable divided grid">
-          <div className="row">
-            <div className="ten wide column main__column">
-              <TopBar />
-              <ShowTasks />
-            </div>
-            <div className="six wide column main__column">
-              <AddTask />
-            </div>
+    const leftSide = this.props.isSelected;
+
+    return (
+      <div className="ui container main stackable divided grid">
+        <Notify ref="notify"/>
+        <div className="row">
+          <div className={`${leftSide ? "ten" : "sixteen"} wide column main__column`}>
+            <TopBar />
+            <ShowTasks />
           </div>
+          {
+            leftSide ? <div className="six wide column main__column"><AddTask /></div> : ""
+          }
         </div>
-      );
+      </div>
+    );
   }
 } );
 
