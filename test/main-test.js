9da4ae3d8aca3b7616002a60aaf8acbb621c409b
Anatoly Gusev <avg.tolik@gmail.com>
2016-04-01 02:19:19 +0300

    Add tests to main file

--- test/main-test.js
+++ test/main-test.js
@@ -3,8 +3,10 @@
 /** Test Models **/
 require( './models/model-access-token' );
 require( './models/model-user' );
+require( './models/model-task' );
 require( './models/model-message' );
 
 /** Test Routes **/
 require( './routes/routes-user' );
 require( './routes/routes-task' );
+require( './routes/routes-message' );
