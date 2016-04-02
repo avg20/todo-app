52b0391cfa9ac216804466c7f9cf16eddd4c19c5
Anatoly Gusev <avg.tolik@gmail.com>
2016-04-02 20:48:03 +0300

    Add async to auth

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,10 @@
 /** auth.js **/
 'use strict';
 
-const AccessToken = require( './models/access_token' );
-const User = require( './models/user' );
-const ObjectId = require( 'mongoose' ).Types.ObjectId;
+const AccessToken = require('./models/access_token');
+const User = require('./models/user');
+const ObjectId = require('mongoose').Types.ObjectId;
+const async = require('async');
 
 /**
  * Authorized user object
@@ -13,31 +14,39 @@ module.exports.user = null;
 /**
  * Init module, check access token from request and find user by access token
  **/
-module.exports.init = ( req, res, next ) => {
+module.exports.init = (req, res, callback) => {
   const token = req.body.token || req.query.token;
-
-  AccessToken.findOne( { token: token }, ( err, accessToken ) => {
-    if ( err ) throw err;
-
-    if ( accessToken != null ) {
-      User.findOne( { _id: new ObjectId( accessToken.user_id ) }, ( err, model ) => {
-        if ( err ) throw err;
-
-        module.exports.user = model;
-        next();
-      } );
-    } else {
-      next();
+  
+  async.waterfall([
+    (next) => {
+      AccessToken.findOne({ token }, next);
+    },
+    (accessToken, next) => {
+      if (accessToken !== null) {
+        User.findOne({ _id: new ObjectId(accessToken.user_id) }, next);
+      } else {
+        next(null);
+      }
+    },
+  ], (err, user) => {
+    if (err) {
+      throw err;
     }
-  } );
+    
+    if (user !== null) {
+      module.exports.user = user;
+    }
+    
+    callback();
+  });
 };
 
 /**
  * Check, if user are authorized
  **/
-module.exports.check = ( req, res, next ) => {
-  if ( module.exports.user === null ) {
-    res.sendStatus( 401 );
+module.exports.check = (req, res, next) => {
+  if (module.exports.user === null) {
+    res.sendStatus(401);
   } else {
     next();
   }
