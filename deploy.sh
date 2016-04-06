607da3223d611cd9d5c958518f6cccacb00d9102
Travis CI <travis@domain.com>
2016-04-06 10:32:30 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-npm build
+gulp build
+echo "Built!"
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "travis@domain.com"
 
-cat .gitignore
 echo "node_modules" > .gitignore
-cat .gitignore
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
