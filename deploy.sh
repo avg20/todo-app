cc2bc0f8e522c39c391a566c4551bdd471e41a18
Anatoly Gusev <avg.tolik@gmail.com>
2016-04-06 09:37:13 +0300

    update deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+npm build
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "travis@domain.com"
+
+rm .gitignore
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add -A
+git commit -m "Deploy to Heroku"
